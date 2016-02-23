dcb3028aaaa0f85844e361e08942abb90c1fe69a
Todd Johnson <todd.johnson@intel.com>
2016-02-23 15:10:08 -0800

    Allow multiple backup files Fix day rollover detection Log uncaught exceptions

--- LoggerTest/AppDelegate.swift
+++ LoggerTest/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        NSSetUncaughtExceptionHandler({ exception in
+            LogMessage.e("Uncaught exception: \(exception)")
+            LogMessage.flushLog()
+        })
         return true
     }
 
--- LoggerTest/LogMessage.swift
+++ LoggerTest/LogMessage.swift
@@ -21,84 +21,109 @@ class LogMessage {
 
     private static let logger = LogMessage()
     private let loggerDateFormatter = NSDateFormatter()
-    private let julianDateFormatter = NSDateFormatter()
     private let messageQueue: dispatch_queue_t
     private var loggingLevel = LogLevel.Info
     private var logfileHandle: NSFileHandle?
+    private var logfileDay: Int64? = nil
+    private var backupDays: UInt = 2
 
     private init() {
         self.loggerDateFormatter.dateFormat = "y-MM-d HH:mm:ss.SSS"
-        self.julianDateFormatter.dateFormat = "g"
         self.messageQueue = dispatch_queue_create("com.toddjohn.LogMessage", DISPATCH_QUEUE_SERIAL)
     }
 
     deinit {
-        self.logfileHandle?.synchronizeFile()
-        self.logfileHandle?.closeFile()
+        self.closeLogFile()
     }
 
     private func closeLogFile() {
         self.logfileHandle?.synchronizeFile()
         self.logfileHandle?.closeFile()
         self.logfileHandle = nil
+        self.logfileDay = nil
     }
 
-    private func getJulianDay(date: NSDate? = nil) -> Int {
-        var formateDate: NSDate! = date
-        if formateDate == nil {
-            formateDate = NSDate()
-        }
+    private func getDay(date: NSDate) -> Int64 {
+        let daySeconds: Int64 = 60 * 60 * 24
 
-        let stringDay = self.julianDateFormatter.stringFromDate(formateDate)
-        return Int(stringDay)!
+        let seconds = Int64(date.timeIntervalSinceReferenceDate)
+        let timeZone = NSTimeZone.localTimeZone()
+        let timeZoneSeconds = Int64(timeZone.secondsFromGMT)
+        let localSeconds = seconds + timeZoneSeconds
+        let day = localSeconds / daySeconds
+
+        return day
     }
 
     private func getLogfileName() -> String {
         return NSHomeDirectory() + "/Documents/CurrentLog.txt"
     }
 
-    private func getBackupFileName() -> String {
-        return NSHomeDirectory() + "/Documents/PreviousLog.txt"
+    private func getBackupFileName(daysAgo: UInt) -> String {
+        return NSHomeDirectory() + "/Documents/PreviousLog\(daysAgo).txt"
     }
 
-    private func backupLogFile() {
+    private func backupLogFile(backupName: String, currentName: String) {
         let fileManager = NSFileManager.defaultManager()
         // delete old backup
-        let backup = self.getBackupFileName()
         do {
-            try fileManager.removeItemAtPath(backup)
-        } catch {
-            NSLog("Error removing backup file: \(error)")
+            try fileManager.removeItemAtPath(backupName)
+        } catch let error as NSError {
+            // Ignore file not found errors
+            if error.code != NSFileNoSuchFileError {
+                NSLog("Error removing backup file: \(error)")
+            }
         }
         // rename current file to backup
-        let current = self.getLogfileName()
         do {
-            try fileManager.moveItemAtPath(current, toPath: backup)
+            try fileManager.moveItemAtPath(currentName, toPath: backupName)
         } catch {
             NSLog("Error moving log file to backup: \(error)")
         }
     }
 
-    private func getFileHandle() -> NSFileHandle {
-        if self.logfileHandle != nil {
+    private func shiftBackupFiles() {
+        if self.backupDays > 1 {
+            let fileRange = 2...self.backupDays
+            for i in fileRange.reverse() {
+                let previous = self.getBackupFileName(i)
+                let next = self.getBackupFileName(i - 1)
+                self.backupLogFile(previous, currentName: next)
+            }
+        }
+        let backup = self.getBackupFileName(1)
+        let current = self.getLogfileName()
+        self.backupLogFile(backup, currentName: current)
+    }
+
+    private func createLogfile(filename: String, today: Int64) {
+        let fileManager = NSFileManager.defaultManager()
+        if fileManager.fileExistsAtPath(filename) {
+            let fileAttributes = try! fileManager.attributesOfItemAtPath(filename)
+            let fileDate = fileAttributes[NSFileCreationDate] as! NSDate
+            let fileDay = self.getDay(fileDate)
+            // check creation date to see if file needs to be migrated
+            if fileDay < today {
+                self.shiftBackupFiles()
+                fileManager.createFileAtPath(filename, contents: nil, attributes: nil)
+            }
+        } else {
+            fileManager.createFileAtPath(filename, contents: nil, attributes: nil)
+        }
+    }
+
+    private func getFileHandle(messageDate: NSDate) -> NSFileHandle {
+        let logfileName = getLogfileName()
+        let today = self.getDay(messageDate)
+        if self.logfileDay < today {
+            self.closeLogFile()
+            self.createLogfile(logfileName, today: today)
+            self.logfileDay = today
+        } else {
+            precondition(self.logfileHandle != nil, "Logfile day is initialized, but file handle is nil")
             return self.logfileHandle!
         }
 
-        let logfileName = getLogfileName()
-        let fileManager = NSFileManager.defaultManager()
-
-        if fileManager.fileExistsAtPath(logfileName) {
-            // check creation date to see if file needs to be migrated
-            let today = self.getJulianDay()
-            let fileDate = try! fileManager.attributesOfItemAtPath(logfileName)[NSFileCreationDate] as! NSDate
-            let fileDay = self.getJulianDay(fileDate)
-            if fileDay < today {
-                self.backupLogFile()
-                fileManager.createFileAtPath(logfileName, contents: nil, attributes: nil)
-            }
-        } else {
-            fileManager.createFileAtPath(logfileName, contents: nil, attributes: nil)
-        }
         self.logfileHandle = NSFileHandle(forWritingAtPath: logfileName)
         self.logfileHandle?.seekToEndOfFile()
 
@@ -110,7 +135,7 @@ class LogMessage {
         let formattedString = "\(timestamp): \(message)"
         print(formattedString)
 
-        let fileHandle = self.getFileHandle()
+        let fileHandle = self.getFileHandle(time)
         let fileString = formattedString + "\n"
         let fileData = fileString.dataUsingEncoding(NSUTF8StringEncoding)
         fileHandle.writeData(fileData!)
@@ -135,6 +160,11 @@ class LogMessage {
 
     static func setLogLevel(level: LogLevel) {
         LogMessage.logger.loggingLevel = level
+    }
+
+    static func setBackupDays(days: UInt) {
+        let maxBackupDays: UInt = 5
+        LogMessage.logger.backupDays = min(days, maxBackupDays)
     }
 
     static func flushLog() {
